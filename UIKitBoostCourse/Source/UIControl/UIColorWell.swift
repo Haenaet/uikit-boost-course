9474a0c2aa20e10338f2a40e2059a62d63c08bf0
yujinkim1 <yujin7621@me.com>
2023-09-08 10:53:58 +0900

    Create UIColorWell.swift
    Initialization

---
+++ UIKitBoostCourse/Source/UIControl/UIColorWell.swift
@@ -0,0 +1,25 @@
+//
+//  UIColorWell.swift
+//  UIKitBoostCourse
+//
+//  Created by Yujin Kim on 2023-09-08.
+//
+
+import UIKit
+
+class UIColorWell: UIControl {
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+    
+}

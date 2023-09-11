b12cd79b93774fbc8e775fa21f89e466e94975a6
real-future <future.ver1@gmail.com>
2023-09-11 21:59:33 +0900

    Create UIButton.swift
    Initialization

---
+++ UIKitBoostCourse/Source/UIControl/UIButton.swift
@@ -0,0 +1,27 @@
+//
+//  UIButton.swift
+//  UIKitBoostCourse
+//
+//  Created by FUTURE on 2023/09/11.
+//
+
+import UIKit
+
+class UIButton: UIControl {
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

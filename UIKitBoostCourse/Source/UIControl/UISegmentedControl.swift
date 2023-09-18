d4e3397073ae5498e2b8d52a861a0b287b03fa2d
yujinkim1 <yujin7621@me.com>
2023-09-18 22:32:46 +0900

    Create UISegmentedControl.swift

---
+++ UIKitBoostCourse/Source/UIControl/UISegmentedControl.swift
@@ -0,0 +1,10 @@
+//
+//  UISegmentedControl.swift
+//  UIKitBoostCourse
+//
+//  Created by Yujin Kim on 2023-09-18.
+//
+
+import UIKit
+
+class UISegmentedControlViewController: BaseUIViewController {}

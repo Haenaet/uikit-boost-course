bb51fdaf11f58c927d0d0fe3fe1912f15d90a269
real-future <future.ver1@gmail.com>
2023-10-08 17:47:43 +0900

    UIPickerView.swift 주석 정리

--- UIKitBoostCourse/Source/UIControl/UIPickerView.swift
+++ UIKitBoostCourse/Source/UIControl/UIPickerView.swift
@@ -10,31 +10,34 @@ import SwiftUI
 
 class UIPickerViewController : BaseUIViewController {
     
+    // 첫 번째 컴포넌트에 표시될 데이터
     let data1 = ["1번 항목", "2번 항목", "3번 항목"]
+    
+    // 두 번째 컴포넌트에 표시될 데이터
     let data2 = ["string형식은", "다 가능✅", "합니다", "짠" ]
     
+    
+    // UIPickerView 인스턴스 생성 및 설정
     lazy var picker: UIPickerView = {
         let p = UIPickerView()
-        p.dataSource = self
-        p.delegate = self
+        p.dataSource = self // 데이터 소스 지정
+        p.delegate = self // 델리게이트 지정
         p.translatesAutoresizingMaskIntoConstraints = false
         return p
     }()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupUI()
+        setUI()
     }
     
-    func setupUI() {
+    
+    override func setUI() {
         self.view.backgroundColor = .white
         self.view.addSubview(picker)
         
-        setPickerConstraints()
-    }
-    
-    func setPickerConstraints() {
         NSLayoutConstraint.activate([
             picker.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             picker.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
@@ -44,12 +47,15 @@ class UIPickerViewController : BaseUIViewController {
     }
 }
 
+
 // UIPickerViewDataSource extension
 extension UIPickerViewController: UIPickerViewDataSource {
+    // 컴포넌트(열)의 수 반환
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
+    // 각 컴포넌트에서 표시될 행의 수 반환
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
         case 0:
@@ -62,10 +68,12 @@ extension UIPickerViewController: UIPickerViewDataSource {
     }
 }
 
+
 // UIPickerViewDelegate extension
 extension UIPickerViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         switch component {
+        // 각 행에 표시될 문자열 반환
         case 0:
             return data1[row]
         case 1:
@@ -75,6 +83,7 @@ extension UIPickerViewController: UIPickerViewDelegate {
         }
     }
 }
+
 
 // 프리뷰
 private struct UIViewControllerRepresenter: UIViewControllerRepresentable {
